d7dc5f1351c17cbebc72095094def56eafa727ce
irinakostikova <65498323+irinakostikova@users.noreply.github.com>
2020-11-25 17:20:39 +0300

    Add files via upload

--- src/main/java/laba1/csv/CSVReaderImpl.java
+++ src/main/java/laba1/csv/CSVReaderImpl.java
@@ -1,41 +1,26 @@
 package laba1.csv;
 
-
 import com.opencsv.CSVReader;
-import laba1.service.Question;
 import org.springframework.stereotype.Component;
-
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import laba1.service.Question;
 
 @Component
 public class CSVReaderImpl implements ICSVReader {
-
-
    private String filePath="questions.csv";
 
     public List<Question> getQuestions() throws IOException {
 
         List<Question> result = new ArrayList<>();
-
-
         InputStream is = CSVReaderImpl.class.getResourceAsStream("/questions.csv");
         CSVReader csvReader = new CSVReader(new InputStreamReader(is));
         String[] values = null;
         while ((values = csvReader.readNext()) != null){
-
             String read= Arrays.toString(values);
             String[] answers = read.split(";");
-
             result.add(new Question(answers[1], answers[2], answers[3]));
         }
-
         return result;
     }
 }
